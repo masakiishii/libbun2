4e412c93b9824673cf8bc7400a1f6395daba48ba
masakiishii <masaki.ishii511@gmail.com>
2014-07-24 15:22:47 +0900

    update Array in JsDriver

--- src/org/libbun/drv/JsDriver.java
+++ src/org/libbun/drv/JsDriver.java
@@ -1,16 +1,19 @@
 package org.libbun.drv;
 
+import org.libbun.BunDriver;
 import org.libbun.BunType;
+import org.libbun.DriverCommand;
 import org.libbun.Functor;
 import org.libbun.Main;
 import org.libbun.Namespace;
 import org.libbun.UMap;
+import org.libbun.drv.JvmDriver.PushAsLong;
 import org.libbun.peg4d.PegObject;
 
 public class JsDriver extends SourceDriver {
 
-    public JsDriver() {
-	
+	public JsDriver() {
+		this.addCommand("NewArray", new NewArray());
     }
 
 	@Override
@@ -32,5 +35,21 @@ public class JsDriver extends SourceDriver {
 	}
 	public void generateMain() {
 		
+	}
+    protected class NewArray extends DriverCommand {
+		@Override
+		public void invoke(BunDriver driver, PegObject node, String[] param) {
+			StringBuffer code = new StringBuffer();
+			int size = node.size();
+			code.append("new Array(");
+			for(int i = 0; i < size; i++) {
+				code.append(node.AST[i].getText());
+				if(i != size - 1) {
+					code.append(", ");
+				}
+			}
+			code.append(")");
+			driver.pushCode(code.toString());
+		}
 	}
 }
